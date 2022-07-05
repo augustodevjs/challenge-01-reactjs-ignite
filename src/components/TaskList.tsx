0cbf112f066171d1f25fc4ebbdd8e89be083d19e
augusto <augustosaaude@gmail.com>
2022-07-04 22:13:28 -0300

    git commit -m

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,16 +14,30 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
+  console.log(tasks);
+
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) {
+      return;
+    }
+
+    setTasks([...tasks, {
+      id: Math.floor(Math.random() * 10000000),
+      title: newTaskTitle,
+      isComplete: false
+    }]);
+
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const editTasks = tasks.map((task) => task.id === id ? { ...task, isComplete: !task.isComplete }: task);
+    setTasks(editTasks);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const removeTasks = tasks.filter((tasks) => tasks.id !== id);
+    setTasks(removeTasks);
   }
 
   return (
